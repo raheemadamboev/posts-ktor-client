693de86ab8f857a9c50088a97b440d166ae42143
Raheem <raheemideadev@gmail.com>
2021-11-07 02:31:30 +0500

    main screen

--- app/src/main/java/xyz/teamgravity/postsktorclient/presentation/MainActivity.kt
+++ app/src/main/java/xyz/teamgravity/postsktorclient/presentation/MainActivity.kt
@@ -3,19 +3,64 @@ package xyz.teamgravity.postsktorclient.presentation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Card
+import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import dagger.hilt.android.AndroidEntryPoint
 import xyz.teamgravity.postsktorclient.presentation.ui.theme.PostsKtorClientTheme
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            PostsKtorClientTheme {
 
+            val viewmodel = hiltViewModel<MainViewModel>()
+            val state = viewmodel.state.value
+
+            PostsKtorClientTheme {
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(Color.LightGray),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    if (state.posts != null) { // success
+                        LazyColumn(modifier = Modifier.fillMaxSize()) {
+                            items(state.posts) {
+                                Card(
+                                    modifier = Modifier
+                                        .padding(horizontal = 10.dp, vertical = 2.5.dp)
+                                        .fillMaxWidth(),
+                                    backgroundColor = Color.White
+                                ) {
+                                    Text(
+                                        text = it.title,
+                                        modifier = Modifier.padding(16.dp)
+                                    )
+                                }
+                            }
+                        }
+
+                    } else {
+                        if (state.loading) {
+                            CircularProgressIndicator()
+                        } else {
+                            state.error?.let { Text(text = it) }
+                        }
+                    }
+                }
             }
         }
     }
