2f9afacfb52e2d3e91d34d36cb4c52d82632f7fe
Raheem <raheemideadev@gmail.com>
2021-11-07 01:32:51 +0500

    inject dependencies

---
+++ app/src/main/java/xyz/teamgravity/postsktorclient/injection/App.kt
@@ -0,0 +1,7 @@
+package xyz.teamgravity.postsktorclient.injection
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class App: Application()
---
+++ app/src/main/java/xyz/teamgravity/postsktorclient/injection/ApplicationModule.kt
@@ -0,0 +1,37 @@
+package xyz.teamgravity.postsktorclient.injection
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import io.ktor.client.*
+import io.ktor.client.engine.android.*
+import io.ktor.client.features.json.*
+import io.ktor.client.features.json.serializer.*
+import io.ktor.client.features.logging.*
+import xyz.teamgravity.postsktorclient.data.remote.PostsApi
+import xyz.teamgravity.postsktorclient.data.remote.PostsApiImpl
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object ApplicationModule {
+
+    @Provides
+    @Singleton
+    fun provideHttpClient(): HttpClient = HttpClient(Android) {
+        install(Logging) {
+            level = LogLevel.ALL
+        }
+
+        install(JsonFeature) {
+            serializer = KotlinxSerializer()
+        }
+    }
+
+    @Provides
+    @Singleton
+    fun providePostsApi(client: HttpClient): PostsApi = PostsApiImpl(client)
+
+
+}
